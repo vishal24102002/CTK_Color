e0f98151c8b438068ead388d20af699b7c5a2926
Vishal Sharma <103731436+vishal24102002@users.noreply.github.com>
2026-01-02 11:53:02 +0530

    Fix formatting of CTK_Color calls in test2.py

--- tests/test2.py
+++ tests/test2.py
@@ -1,11 +1,11 @@
 from CTK_Color import CTK_Color
 import customtkinter as ctk
 
-win=ctk.CTk()
-text=ctk.CTkTextbox(win)
+win = ctk.CTk()
+text = ctk.CTkTextbox(win)
 text.pack()
-CTK_Color(text,"for","bal","red").for_word()
-CTK_Color(text,"print","edit","orange").for_word()
-CTK_Color(text,"while","gang","blue").for_word()
-CTK_Color(text,"boxer","baler","green").for_word()
+CTK_Color(text, "for", "bal", "red").for_word()
+CTK_Color(text, "print", "edit", "orange").for_word()
+CTK_Color(text, "while", "gang", "blue").for_word()
+CTK_Color(text, "boxer", "baler", "green").for_word()
 win.mainloop()

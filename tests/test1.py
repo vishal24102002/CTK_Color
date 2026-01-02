7b6aa5c6287e29cb4b6699f8bfd6e7ed29d4a622
Vishal Sharma <103731436+vishal24102002@users.noreply.github.com>
2026-01-02 11:51:02 +0530

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -1,8 +1,8 @@
 from CTK_Color import CTK_Color
 import customtkinter as ctk
 
-win=ctk.CTk()
-text=ctk.CTkTextbox(win)
+win = ctk.CTk()
+text = ctk.CTkTextbox(win)
 text.pack()
-CTK_Color(text,"box","big","red").for_line()
+CTK_Color(text, "box", "big", "red").for_line()
 win.mainloop()
